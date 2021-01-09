f82f09f356c5431450a80634df0e8d19d2091999
Robin Timmerman <robintimmerman@hotmail.nl>
2021-01-09 21:40:42 +0100

    spelerbeweging
    scene en speler aangemaakt en speler kan rondkijken en bewegen

---
+++ Game/Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour
+{
+    public float sensitivity = 100;
+    float xRotation = 0;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
