ee7d9907970e1f1721b01bf3ec35621531bb03dd
Robin Timmerman <robintimmerman@hotmail.nl>
2021-02-07 16:51:53 +0100

    heen en weer
    scriptje voor heen en weer bewegen, zie het platfrom in de testscène voor een voorbeeld
    objecten op het platform bewegen met het platform mee

---
+++ Game/Assets/Scripts/AttachObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachObject : MonoBehaviour  // Dit kan worden aangepast om alleen bepaalde opjecten vast te kunnen maken
+{
+    Dictionary<Collider, Transform> oldParents = new Dictionary<Collider, Transform>();
+
+    void OnTriggerEnter(Collider other)
+    {
+        oldParents.Add(other, other.transform.parent);
+        other.gameObject.transform.parent = transform.parent;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.transform.parent = oldParents[other];
+        oldParents.Remove(other);
+    }
+}
---
+++ Game/Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UIElements;
+
+public class BackAndForth : MonoBehaviour
+{
+    public enum Axis { x, y, z };
+    public Axis direction;
+    private float startPos;
+    public float distance;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (direction)
+        {
+            case Axis.x:
+                startPos = transform.position.x;
+                break;
+            case Axis.y:
+                startPos = transform.position.y;
+                break;
+            case Axis.z:
+                startPos = transform.position.z;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (direction)
+        {
+            case Axis.x:
+                transform.Translate(transform.right * speed * Time.deltaTime);
+                if (transform.position.x > startPos + distance && speed > 0 || transform.position.x < startPos && speed < 0) { speed = -speed; }
+                break;
+            case Axis.y:
+                transform.Translate(transform.up * speed * Time.deltaTime);
+                if (transform.position.y > startPos + distance && speed > 0 || transform.position.y < startPos && speed < 0) { speed = -speed; }
+                break;
+            case Axis.z:
+                transform.Translate(transform.forward * speed * Time.deltaTime);
+                if (transform.position.z > startPos + distance && speed > 0 || transform.position.z < startPos && speed < 0) { speed = -speed; }
+                break;
+        }
+    }
+}
